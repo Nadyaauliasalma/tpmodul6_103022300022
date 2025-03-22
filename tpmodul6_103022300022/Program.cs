5e371d89c23236a9c540599378f86fc8b9f67482
Nadya Aulia <nadyaauliasalma622@gmail.com>
2025-03-22 19:52:55 +0700

    implementasi design by contract

--- tpmodul6_103022300022/Program.cs
+++ tpmodul6_103022300022/Program.cs
@@ -5,8 +5,24 @@ class Program
 {
     static void Main()
     {
-        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract – [Nadya]");
-        video.IncreasePlayCount(10);
-        video.PrintVideoDetails();
+        try
+        {
+            SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract – [Nadya]");
+            video.PrintVideoDetails();
+
+            video.IncreasePlayCount(5000);
+            video.PrintVideoDetails();
+
+            for (int i = 0; i < 10; i++)
+            {
+                video.IncreasePlayCount(10000000);
+            }
+
+            video.PrintVideoDetails();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Exception: {e.Message}");
+        }
     }
 }
--- tpmodul6_103022300022/SayaTubeVideo.cs
+++ tpmodul6_103022300022/SayaTubeVideo.cs
@@ -14,6 +14,11 @@ namespace tpmodul6_103022300022
 
         public SayaTubeVideo(string title)
         {
+            if (title == null || title.Length > 100)
+            {
+                throw new ArgumentException("Judul video tidak boleh null dan maksimal 100 karakter.");
+            }
+
             Random rand = new Random();
             this.id = rand.Next(10000, 99999);
             this.title = title;
@@ -22,7 +27,22 @@ namespace tpmodul6_103022300022
 
         public void IncreasePlayCount(int count)
         {
-            playCount += count;
+            if (count > 10000000)
+            {
+                throw new ArgumentException("Penambahan play count maksimal 10.000.000.");
+            }
+
+            try
+            {
+                checked
+                {
+                    playCount += count;
+                }
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Error: Terjadi overflow pada play count.");
+            }
         }
 
         public void PrintVideoDetails()
